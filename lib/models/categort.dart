28aae5fb3a2a1ae68b34b2ac9781b0d220c3882f
Khanh Long Nguyen <khanhlong1301@Suijin.local>
2023-03-02 14:48:11 +0700

    Classes and Constructor

---
+++ lib/models/categort.dart
@@ -0,0 +1,15 @@
+class Category {
+  String id;
+  String name;
+  bool? isChecked;
+
+  // Category(this.id, this.name, this.isChecked); //constructor
+  Category({
+    required this.id,
+    required this.name,
+    this.isChecked,
+  }); //constructor
+}
+
+// var category = Category('1', 'All', true);
+var category = Category(id: '1', name: 'All', isChecked: true);
